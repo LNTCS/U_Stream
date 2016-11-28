60c3cb3414fb60e20fd101ded878e75bb719e901
LNTCS <lcs7958@gmail.com>
2016-11-28 11:33:17 +0900

    메인 화면 구성 및 초기 값 설정

--- app/src/main/java/kr/edcan/u_stream/MainActivity.kt
+++ app/src/main/java/kr/edcan/u_stream/MainActivity.kt
@@ -2,11 +2,14 @@ package kr.edcan.u_stream
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.toolbar_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbarTitle.text = "플레이보드"
     }
 }
