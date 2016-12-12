f404ad3a2e3ec6e025d2c26d7039c1d00205e3a1
LNTCS <lcs7958@gmail.com>
2016-12-12 15:06:06 +0900

    커스텀 노티피케이션, 재생/일시정지 버튼

--- app/src/main/java/kr/edcan/u_stream/PlayService.kt
+++ app/src/main/java/kr/edcan/u_stream/PlayService.kt
@@ -2,9 +2,12 @@ package kr.edcan.u_stream
 
 import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.databinding.ObservableArrayList
 import android.media.AudioManager
 import android.media.MediaPlayer
@@ -12,9 +15,12 @@ import android.net.Uri
 import android.os.IBinder
 import android.support.v4.app.NotificationCompat
 import android.util.SparseArray
+import android.widget.RemoteViews
 import android.widget.TextView
 import at.huber.youtubeExtractor.YouTubeUriExtractor
 import at.huber.youtubeExtractor.YtFile
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.NotificationTarget
 import kr.edcan.u_stream.model.MusicData
 import org.jetbrains.anko.toast
 import java.util.*
@@ -56,6 +62,8 @@ class PlayService : Service() {
         }
     }
 
+    var beforeEvent: Long = 0
+    var playable = true
     var mediaPlayer: MediaPlayer
     var mContext: Context
     var notification by Delegates.notNull<Notification>()
@@ -71,11 +79,22 @@ class PlayService : Service() {
     }
 
     override fun onCreate() {
+        val view = RemoteViews(mContext.packageName, R.layout.content_notification)
+        view.setImageViewResource(R.id.notifyPlay, if (mediaPlayer.isPlaying) R.drawable.selector_notify_pause else R.drawable.selector_notify_play)
+
         builder = NotificationCompat.Builder(this)
                 .setContentText("μ'Stream")
                 .setSmallIcon(R.drawable.ic_noti)
+                .setCustomContentView(view)
+
         notification = builder.build()
         manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//        val i = Intent(mContext, PlayerActivity::class.java)
+//        i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//        i.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+//        val pi = PendingIntent.getActivity(mContext, 0, i, PendingIntent.FLAG_UPDATE_CURRENT)
+//        notification.contentIntent = pi
+        setIntent(view)
         super.onCreate()
     }
 
@@ -87,6 +106,7 @@ class PlayService : Service() {
             ACTION_START -> {
                 mediaPlayer.reset()
                 mediaPlayer.setOnPreparedListener {
+                    playable = true
                     mediaPlayer.start()
                     startNotification()
                 }
@@ -113,6 +133,7 @@ class PlayService : Service() {
     fun startNotification(){
         builder.setContentTitle("재생중")
         notification = builder.build()
+        updateNotification()
         this.startForeground(NOTIFICATION_NUM, notification)
     }
 
@@ -120,6 +141,7 @@ class PlayService : Service() {
         this.stopForeground(false)
         builder.setContentTitle("멈춰라 얍!")
         notification = builder.build()
+        updateNotification()
         manager.notify(NOTIFICATION_NUM, notification)
     }
 
@@ -132,6 +154,7 @@ class PlayService : Service() {
     }
 
     fun play(){
+        playable = false
         ytEx = object : YouTubeUriExtractor(mContext) {
             override fun onUrisAvailable(videoId: String, videoTitle: String, ytFiles: SparseArray<YtFile>?) {
                 if (ytFiles != null) {
@@ -154,6 +177,55 @@ class PlayService : Service() {
             }
         }
         ytEx!!.execute("https://www.youtube.com/watch?v=${nowPlaying!!.videoId}")
+    }
+
+    fun updateNotification(){
+        val rv = notification.contentView
+        rv?.let{
+            rv.setTextViewText(R.id.notifyTitle, nowPlaying.title)
+            rv.setTextViewText(R.id.notifySubtitle, nowPlaying.uploader)
+            rv.setImageViewResource(R.id.notifyPlay, if (mediaPlayer.isPlaying) R.drawable.selector_notify_pause else R.drawable.selector_notify_play)
+            val notificationTarget = NotificationTarget(mContext, rv, R.id.notifyThumb, notification, NOTIFICATION_NUM)
+            Glide.with(mContext).load(nowPlaying.thumbUri).asBitmap().placeholder(R.drawable.ic_notify_album).into(notificationTarget)
+        }
+    }
+
+    fun setIntent(views: RemoteViews) {
+        val playIntent = Intent("kr.edcan.ustream.control.play")
+        val pendingIntent = PendingIntent.getBroadcast(mContext, 0, playIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        views.setOnClickPendingIntent(R.id.notifyPlay, pendingIntent)
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("kr.edcan.ustream.control.play")
+        mContext.registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val currentEvent = System.currentTimeMillis()
+                if (beforeEvent < currentEvent - 100 && playable) {
+                    if (mediaPlayer.isPlaying)
+                        mediaPlayer.pause()
+                    else
+                        mediaPlayer.start()
+                }
+                beforeEvent = currentEvent
+                updateNotification()
+            }
+        }, intentFilter)
+
+        val forwardIntent = Intent("kr.edcan.ustream.control.forward")
+        val pendingIntentF = PendingIntent.getBroadcast(mContext, 0, forwardIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        views.setOnClickPendingIntent(R.id.notifyNext, pendingIntentF)
+        val intentFilterF = IntentFilter()
+        intentFilterF.addAction("kr.edcan.ustream.control.forward")
+        mContext.registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val currentEvent = System.currentTimeMillis()
+                if (beforeEvent < currentEvent - 100 && playable) {
+                    // TODO 다음곡 재생
+                }
+                beforeEvent = currentEvent
+            }
+        }, intentFilterF)
     }
 
 }
